22254759313e3ca23848d186c82dfe38f7a81803
jlgerhardt <jlgerhardt@ucdavis.edu>
2024-06-04 00:02:25 -0700

    version2

---
+++ development/version2/script.js
@@ -0,0 +1,49 @@
+( function (){
+    'use strict';
+
+    // CODE FOR DISPLAYING TIME
+    setInterval(function() {
+        // Just move your date creation inside the interval function
+        var today = new Date();
+        // var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        // var dateTime = date + " " + time; // Add the time to the date string
+      
+        // Now it will take the current date and put it in all html elements
+        document.getElementById('clock').innerHTML = time;
+
+      }, 1000);
+
+
+
+    // CODE FOR HIDING AND SHOWING CONTENT
+    const link1 = document.getElementById("link1");
+
+    link1.addEventListener("click", function(event) {
+        event.preventDefault();
+        document.getElementById("header").innerHTML = `<h1> Where can YOU live comfortably? </h1> `;
+        document.getElementById("content").innerHTML = 
+        `<section>
+        <p>Enter Your Projected Income Range:</p> <br>
+        <input type="text" id="#" name="#" value="Income">
+        <a class= "continue" id="link2" href="#"> NEXT </a>
+        </section>
+        `;
+    });
+
+    document.getElementById("content").addEventListener("click", function(event) {
+        if (event.target && event.target.id === "link2") {
+            event.preventDefault();
+            document.getElementById("header").innerHTML = `
+            <div class="wrapper">
+            <p>Click on the states for more information. <br> Press ESC to reset zoom.</p>
+            <p>Your Income Range: <br> <b>$00,000-$00,000</p>
+            </div>
+            `;
+            document.getElementById("content").innerHTML = ``;
+            document.getElementById("observablehq-chart-0997c195").classList.remove('notThere');
+            document.getElementById("observablehq-chart-0997c195").classList.add('There');
+        }
+    });  
+
+})();
